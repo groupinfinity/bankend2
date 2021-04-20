444c13977b7b67b40042d5296f4aebca2a01c9f7
Vaib1012 <56437283+Vaib1012@users.noreply.github.com>
2021-04-20 09:42:53 +0530

    changes till the mentor meet

---
+++ src/main/java/com/atrs/app/ArbitrageRecommendationApplication.java
@@ -0,0 +1,13 @@
+package com.atrs.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArbitrageRecommendationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArbitrageRecommendationApplication.class, args);
+	}
+
+}
